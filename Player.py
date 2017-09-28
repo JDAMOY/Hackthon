d59575be4657ad12f0ffa1b0f004085b20695b53
JD <jes@cn.tradeshift.com>
2017-09-28 16:28:47 +0800

    sync with local

--- Player.py
+++ Player.py
@@ -1,22 +1,43 @@
+#coding:utf-8
 import RPi.GPIO as GPIO
 import time
-import subprocess
+import os
+import num2chinese
 
 CHANNEL = 11
 GAP	= 5
+
+musics = {
+        "SUCCESS": "success_zhouxx.mp3",
+        "FAILURE": "failure_kxzsn.wav"
+        }
 
 class Player:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(CHANNEL, GPIO.OUT)
+        GPIO.cleanup()
+    
+    def playTimeAlert(self, hour):
+        htext = num2chinese.num2chinese(hour)
+        htext = "十八"
+        message = "现在是北京时间%s点整" % htext
+        print message
+        self.playSound(message)
 
-    def playMusic(self):
-        pass
+    def playMusic(self, success=True):
+        music = None
+        if success:
+            music = musics['SUCCESS']
+        else:
+            music = musics['FAILURE']
+        os.system('mplayer ' + music)
 
     def playSound(self, words, accend="xiaoyan"):
         cmd = "tts temp.wav '%s' %s && mplayer temp.wav && rm temp.wav " % (words, accend)
         print cmd
-        subprocess.call(cmd)
+        os.system(cmd)
+
 
     def playFlash(self):
         GPIO.output(CHANNEL, 1)
@@ -26,4 +47,7 @@ class Player:
 
 if __name__ == "__main__":
     p = Player()
-    p.playSound("Hehe", "xiaoxin")
+    #print "Play Sound Hehe"
+    #p.playSound("中华人民共和国，今天成立了", "vixqa")
+    #p.playMusic(False)
+    p.playTimeAlert(18)
